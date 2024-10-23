f09bea68ef17fa8acb582972d4e71d1617d2db1f
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2024-10-23 22:20:18 +0000

    commit change

--- models/marts/order_items.sql
+++ models/marts/order_items.sql
@@ -50,7 +50,8 @@ joined as (
         products.is_food_item,
         products.is_drink_item,
 
-        order_supplies_summary.supply_cost
+        order_supplies_summary.supply_cost, 
+        case when order_items.product_id = 'BEV-004' then 'BEV-XXX' end as product_id_update
 
     from order_items
 
